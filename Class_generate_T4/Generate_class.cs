17e62f15809c55240d2d7046b6aeab0824a70e62
Shakir <shakirfarajullayev@gmail.com>
2022-12-25 22:32:55 +0400

    last changes

--- Class_generate_T4/Generate_class.cs
+++ Class_generate_T4/Generate_class.cs
@@ -1,1 +1,244 @@
-﻿ErrorGeneratingOutput
+﻿  
+namespace Class_generate_T4.Models;
+
+public class Employees
+{
+
+    public int EmployeeID { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string? Title { get; set; }
+
+    public string? TitleOfCourtesy { get; set; }
+
+    public DateTime? BirthDate { get; set; }
+
+    public DateTime? HireDate { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Region { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? HomePhone { get; set; }
+
+    public string? Extension { get; set; }
+
+    public byte[]? Photo { get; set; }
+
+    public string? Notes { get; set; }
+
+    public int? ReportsTo { get; set; }
+
+    public string? PhotoPath { get; set; }
+
+
+}
+public class Categories
+{
+
+    public int CategoryID { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public byte[]? Picture { get; set; }
+
+
+}
+public class Customers
+{
+
+    public string CustomerID { get; set; } = null!;
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? ContactName { get; set; }
+
+    public string? ContactTitle { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Region { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Fax { get; set; }
+
+
+}
+public class Shippers
+{
+
+    public int ShipperID { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? Phone { get; set; }
+
+
+}
+public class Suppliers
+{
+
+    public int SupplierID { get; set; }
+
+    public string CompanyName { get; set; } = null!;
+
+    public string? ContactName { get; set; }
+
+    public string? ContactTitle { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? City { get; set; }
+
+    public string? Region { get; set; }
+
+    public string? PostalCode { get; set; }
+
+    public string? Country { get; set; }
+
+    public string? Phone { get; set; }
+
+    public string? Fax { get; set; }
+
+    public string? HomePage { get; set; }
+
+
+}
+public class Orders
+{
+
+    public int OrderID { get; set; }
+
+    public string? CustomerID { get; set; }
+
+    public int? EmployeeID { get; set; }
+
+    public DateTime? OrderDate { get; set; }
+
+    public DateTime? RequiredDate { get; set; }
+
+    public DateTime? ShippedDate { get; set; }
+
+    public int? ShipVia { get; set; }
+
+    public decimal? Freight { get; set; }
+
+    public string? ShipName { get; set; }
+
+    public string? ShipAddress { get; set; }
+
+    public string? ShipCity { get; set; }
+
+    public string? ShipRegion { get; set; }
+
+    public string? ShipPostalCode { get; set; }
+
+    public string? ShipCountry { get; set; }
+
+
+}
+public class Products
+{
+
+    public int ProductID { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public int? SupplierID { get; set; }
+
+    public int? CategoryID { get; set; }
+
+    public string? QuantityPerUnit { get; set; }
+
+    public decimal? UnitPrice { get; set; }
+
+    public short? UnitsInStock { get; set; }
+
+    public short? UnitsOnOrder { get; set; }
+
+    public short? ReorderLevel { get; set; }
+
+    public bool Discontinued { get; set; }
+
+
+}
+public class OrderDetails
+{
+
+    public int OrderID { get; set; }
+
+    public int ProductID { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public short Quantity { get; set; }
+
+    public decimal Discount { get; set; }
+
+
+}
+public class CustomerCustomerDemo
+{
+
+    public string CustomerID { get; set; } = null!;
+
+    public string CustomerTypeID { get; set; } = null!;
+
+
+}
+public class CustomerDemographics
+{
+
+    public string CustomerTypeID { get; set; } = null!;
+
+    public string? CustomerDesc { get; set; }
+
+
+}
+public class Region
+{
+
+    public int RegionID { get; set; }
+
+    public string RegionDescription { get; set; } = null!;
+
+
+}
+public class Territories
+{
+
+    public string TerritoryID { get; set; } = null!;
+
+    public string TerritoryDescription { get; set; } = null!;
+
+    public int RegionID { get; set; }
+
+
+}
+public class EmployeeTerritories
+{
+
+    public int EmployeeID { get; set; }
+
+    public string TerritoryID { get; set; } = null!;
+
+
+}
--- Class_generate_T4/Program.cs
+++ Class_generate_T4/Program.cs
@@ -5,15 +5,23 @@ using System.Text;
 using Microsoft.Data.SqlClient;
 using System.Globalization;
 using System.Threading.Tasks.Dataflow;
-using Pluralize.NET;
+using System.Data.Entity.Design.PluralizationServices;
+using PluralizationService;
+
+using System.Security.Cryptography;
+using System.Runtime.CompilerServices;
 
 namespace Class_generate_T4
 {
     public class Proqram
 
     {
+
         public static void Main()
         {
+
+            //          
+
             Console.OutputEncoding = Encoding.UTF8;
             string strcon = @" Data Source=DESKTOP-DUE6JJJ; 
                                 Initial Catalog=Northwind; 
@@ -22,63 +30,106 @@ namespace Class_generate_T4
                                     TrustServerCertificate=True;";
             SqlConnection sqlConnection = new SqlConnection(strcon);
             sqlConnection.Open();
-            SqlDataAdapter adapter = new SqlDataAdapter("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'", sqlConnection);
-            DataTable dataTable = new DataTable();
-            adapter.Fill(dataTable);
-            List<string> tables = new List<string>();
-            foreach (DataRow row in dataTable.Rows)
+            var sqlData = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'";
+            SqlCommand sqlCommand= new SqlCommand(sqlData,sqlConnection);
+            var tableNames = sqlCommand.ExecuteReader();
+            while(tableNames.Read())
             {
-                IPluralize pluralizer = new Pluralizer();
-                string tablename = pluralizer.Singularize((string)row[0]);
-                tables.Add(tablename);
-                Console.WriteLine(tablename);
+                
+                Console.WriteLine(tableNames[0].ToString().Replace(" ", ""));
+            }
+            
+            
+            
+            
+            
+            //SqlDataAdapter adapter = new SqlDataAdapter("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE'", sqlConnection);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+            //List<string> tables = new List<string>();
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+
+            //    string tablename = (string)row[0];
+            //    tables.Add(tablename);
+            //    Console.WriteLine(tablename);
+            //}
+
+
+            //foreach (string tableName in tables)
+            //{
+            //    adapter = new SqlDataAdapter($@"SELECT CONCAT(
+            //                     'public ',
+            //                     IIF(DATA_TYPE = 'nvarchar', 'string' +         (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'uniqueidentifier', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'ntext', 'string' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'nchar', 'string' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'int', 'int' +                 (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'smallint', 'short' +          (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'tinyint', 'byte' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'money', 'decimal' +           (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'float', 'float' +             (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'decimal', 'decimal' +         (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'bit', 'bool' +                (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'datetime', 'DateTime' +       (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'date', 'DateTime' +           (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'datetime2', 'DateTime' +      (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     IIF(DATA_TYPE = 'image', 'byte[]' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
+            //                     ' ',
+            //                     COLUMN_NAME,'{{ get; set; }}' + (IIF((DATA_TYPE = 'nvarchar' or DATA_TYPE = 'nchar') and (IS_NULLABLE = 'NO'), ' = null!;', ''))
+            //                 )
+            //    FROM INFORMATION_SCHEMA.COLUMNS
+            //    WHERE TABLE_NAME = N'{tableName}'
+            //          and TABLE_SCHEMA = 'dbo'", sqlConnection);
+            //    DataTable dtable = new DataTable();
+            //    adapter.Fill(dtable);
+            //    List<string> columns = new List<string>();
+            //    foreach (DataRow row in dtable.Rows)
+            //    {
+            //        string columnName = (string)row[0];
+            //        columns.Add(columnName);
+            //        Console.WriteLine(columnName);
+
+            //    }
+
+
+            //    sqlConnection.Close();
             }
 
-            //            foreach (string tableName in tables)
-            //            {
-            //                adapter = new SqlDataAdapter($@"SELECT CONCAT(
-            //                 'public ',
-            //                 IIF(DATA_TYPE = 'nvarchar', 'string' +         (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'uniqueidentifier', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'ntext', 'string' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'nchar', 'string' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'int', 'int' +                 (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'smallint', 'short' +          (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'tinyint', 'byte' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'money', 'decimal' +           (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'float', 'float' +             (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'decimal', 'decimal' +         (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'bit', 'bool' +                (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'datetime', 'DateTime' +       (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'date', 'DateTime' +           (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'datetime2', 'DateTime' +      (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 IIF(DATA_TYPE = 'image', 'byte[]' +            (IIF(IS_NULLABLE = 'YES', '?', '')), ''),
-            //                 ' ',
-            //                 COLUMN_NAME,'{{ get; set; }}' + (IIF((DATA_TYPE = 'nvarchar' or DATA_TYPE = 'nchar') and (IS_NULLABLE = 'NO'), ' = null!;', ''))
-            //             )
-            //FROM INFORMATION_SCHEMA.COLUMNS
-            //WHERE TABLE_NAME = N'{tableName}'
-            //      and TABLE_SCHEMA = 'dbo'", sqlConnection);
-            //                DataTable dtable = new DataTable();
-            //                adapter.Fill(dtable);
-            //                List<string> columns = new List<string>();
-            //                foreach (DataRow row in dtable.Rows)
-            //                {
-            //                    string columnName = (string)row[0];
-            //                    columns.Add(columnName);
-            //                    Console.WriteLine(columnName);
 
-            //                }
-
-
-            //                sqlConnection.Close();
-            //            }
-
-            
         }
 
     }
-}
+    
+
+
+ //           {
+ //               var Singular = PluralizationService.CreateService(new System.Globalization.CultureInfo("en-us"));
+
+//               return Singular.Singularize(tablename);
+//           }
+//           string connectionString = @"Server=MACHINE; Database=Northwind; Trusted_Connection=True;";
+//           SqlConnection sqlConnection = new SqlConnection(connectionString);
+//           sqlConnection.Open();
+//           var sql = "SELECT TABLE_NAME\r\n                FROM INFORMATION_SCHEMA.TABLES\r\n                WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_CATALOG='Northwind'";
+//           SqlCommand command = new SqlCommand(sql, sqlConnection);
+//           var data = command.ExecuteReader();
+//           while (data.Read())
+
+//Singularize(data[0].ToString()).Replace(" ", "");
+
+
+//               var propertySql = $"SELECT CONCAT(\r\n                 'public ',\r\n     IIF(DATA_TYPE = 'nvarchar', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n     IIF(DATA_TYPE = 'real', 'Single' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n             IIF(DATA_TYPE = 'uniqueidentifier', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'ntext', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'nchar', 'string' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'int', 'int' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'smallint', 'short' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'tinyint', 'byte' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'money', 'decimal' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'float', 'float' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'decimal', 'decimal' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'bit', 'bool' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'datetime', 'DateTime' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'date', 'DateTime' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'datetime2', 'DateTime' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 IIF(DATA_TYPE = 'image', 'byte[]' + (IIF(IS_NULLABLE = 'YES', '?', '')), ''),\r\n                 ' ',\r\n                 COLUMN_NAME,\r\n                 '  “{{“ get; set; “}}” '\r\n                 + (IIF((DATA_TYPE = 'nvarchar' or DATA_TYPE = 'nchar') and (IS_NULLABLE = 'NO'), ' = null!;', ''))\r\n             )\r\nFROM INFORMATION_SCHEMA.COLUMNS\r\nWHERE TABLE_NAME = N'{data[0]}'     \r\n      and TABLE_SCHEMA = 'dbo'\r\n--SELECT  * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'Northwind'"; #>
+
+//		SqlConnection sqlConnection2 = new SqlConnection(connectionString);
+//               sqlConnection2.Open();
+//               SqlCommand command2 = new SqlCommand(propertySql, sqlConnection2);
+//               var properties = command2.ExecuteReader();
+//               while (properties.Read())
+
+//                   properties[0].ToString().Replace("“", " ").Replace("”", " ");
+
+//               sqlConnection.Close();
 
 
 
